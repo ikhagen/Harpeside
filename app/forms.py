582c254f5b9437135f628490d5303e0e3ce40aba
Ida Hagen <idakhagen@gmail.com>
2017-12-23 12:09:34 +0100

    Email works again; link to part of website

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,8 @@
 from flask_wtf import Form
-from wtforms import StringField, IntegerField
+from wtforms import StringField
 from wtforms.validators import DataRequired
 
 
 class ContactForm(Form):
-    name = StringField('name')
     email = StringField('email', validators=[DataRequired()])
-    phone = IntegerField('phone')
     message = StringField('message')
